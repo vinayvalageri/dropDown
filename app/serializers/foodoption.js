b4c7ca2d540e8772c11bebc1e935d5a02d69c79d
vinayvalageri <vinay.1si13cs131@gmail.com>
2018-02-23 03:53:46 +0530

    food

--- app/serializers/foodoption.js
+++ app/serializers/foodoption.js
@@ -1,7 +1,7 @@
 export default DS.RESTSerializer.extend({
   normalizeResponse(store,primaryModelClass,payload,id,requestType) {
     payload={foodoptions:  payload.foodoption,
-            fooddropdown: payload.dropdown};
+              dropdowns: payload.dropdown};
     console.log(payload);
     return this._super(store,primaryModelClass,payload,id,requestType);
   }
